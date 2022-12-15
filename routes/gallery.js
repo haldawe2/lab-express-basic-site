3513d488245e72845651d514edb84e14a3bbd003
Gerard Guasch <haldawe@hotmail.com>
2022-12-15 18:59:37 +0100

    managed to import the data for the gallery webpage

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const fs = require("fs");
+const img = require('../data/galleryData.js')
 
 router.get('/', function(req, res, next) {
-  const img2 = [
-    {
-      img: 'images/artur-nakhodkin-warhammer-40k-7.jpg',
-      description: "Dark Angels fighting orks"
-    },
-    {
-      img: "images/gallery-artic-world.jpg",
-      description: "Dark Angels fighting on an artic planet"
-    },
-    {
-      img: "images/gallery-chapter-fighting.jpg",
-      description: "Dark Angels fighting world eaters"
-    },
-    {
-      img: "images/gallery-heresy.webp",
-      description: "Dark Angels cleansing heresy"
-    }
-  ]
-
-  res.render('gallery', {img2});
+  res.render('gallery', {img});
 });
 
 module.exports = router;
